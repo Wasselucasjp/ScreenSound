e19e3ff96fd2fb6c9c64243617bcdf4342d21863
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-14 16:10:35 -0300

    instanciando scanner para leitura

--- src/main/java/br/com/wlbr/screensound/principal/Principal.java
+++ src/main/java/br/com/wlbr/screensound/principal/Principal.java
@@ -1,12 +1,19 @@
 package br.com.wlbr.screensound.principal;
 
+import java.util.Scanner;
+
 public class Principal {
+    private Scanner leitura =  new Scanner(System.in);
     public void exibeMenu() {
         var opcao = -1;
         while (opcao != 0){
             var menu = """
                     
                     """;
+
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
         }
     }
 }
