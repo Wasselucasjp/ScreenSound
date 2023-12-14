b36f88228053619c7c3b2cc191c256429352e85a
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-14 15:37:55 -0300

    criando esqueleto do menu de exibicao

---
+++ src/main/java/br/com/wlbr/screensound/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.com.wlbr.screensound.principal;
+
+public class Principal {
+    public void exibeMenu() {
+        var opcao = -1;
+        while (opcao != 0){
+            var menu = """
+                    
+                    """;
+        }
+    }
+}
