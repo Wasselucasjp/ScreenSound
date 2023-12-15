a80f70bec1c6484598f3741916f9f282ac336f6d
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-15 10:56:49 -0300

    modelando classe musicas

--- src/main/java/br/com/wlbr/screensound/model/Musicas.java
+++ src/main/java/br/com/wlbr/screensound/model/Musicas.java
@@ -1,2 +1,27 @@
-package br.com.wlbr.screensound.model;public class Musicas {
+package br.com.wlbr.screensound.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "musicas")
+public class Musicas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    @ManyToOne()
+    private Artista artista;
+
+    @Override
+    public String toString() {
+        return
+                "Musicas='" + titulo +'\'' +
+                ", artista=" + artista;
+    }
 }
