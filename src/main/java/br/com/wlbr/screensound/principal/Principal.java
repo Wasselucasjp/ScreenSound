98a29ea692c8b3fa5fa6bed9fc485ce3cf422aef
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-18 20:55:45 -0300

    criando metodo cadastrarArtista

--- src/main/java/br/com/wlbr/screensound/principal/Principal.java
+++ src/main/java/br/com/wlbr/screensound/principal/Principal.java
@@ -1,9 +1,19 @@
 package br.com.wlbr.screensound.principal;
 
+import br.com.wlbr.screensound.model.Artista;
+import br.com.wlbr.screensound.model.TipoArtista;
+import br.com.wlbr.screensound.repository.ArtistaRepository;
+
 import java.util.Scanner;
 
 public class Principal {
+    private final ArtistaRepository repositorio;
     private Scanner leitura =  new Scanner(System.in);
+
+    public Principal(ArtistaRepository repositorio) {
+        this.repositorio = repositorio;
+    }
+
     public void exibeMenu() {
         var opcao = -1;
         while (opcao != 0){
@@ -27,6 +37,18 @@ public class Principal {
     }
 
     private void cadastrarArtistas() {
-        System.out.println();
+
+        var cadastrarNovo = "S";
+        while (cadastrarNovo.equalsIgnoreCase("S")) {
+            System.out.println("Informe o nome desse artista: ");
+            var nome = leitura.nextLine();
+            System.out.println("informe o tipo desse artista : (solo, dupla ou banda) ");
+            var tipo = leitura.nextLine();
+            TipoArtista tipoArtista = TipoArtista.valueOf(tipo.toUpperCase());
+            Artista artista = new Artista(nome, tipoArtista);
+            repositorio.save(artista);
+            System.out.println("Cadastrar novo artista? (S/N)");
+            cadastrarNovo = leitura.nextLine();
+        }
     }
 }
