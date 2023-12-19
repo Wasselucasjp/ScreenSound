9b43bf15c6a0bd2261459f963d3813e9b138ffc1
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-18 23:44:43 -0300

    implementando metodo para  buscar musica pelo nome

--- src/main/java/br/com/wlbr/screensound/repository/ArtistaRepository.java
+++ src/main/java/br/com/wlbr/screensound/repository/ArtistaRepository.java
@@ -3,5 +3,8 @@ package br.com.wlbr.screensound.repository;
 import br.com.wlbr.screensound.model.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
+    Optional<Artista> findByNomeContainingIgnoreCase(String nome);
 }
