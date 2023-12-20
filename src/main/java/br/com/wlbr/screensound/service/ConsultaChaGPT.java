fb7e988f74e94a35edd3561f1e6ab7487184c9f2
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-20 15:28:33 -0300

    servico de consulta chatgpt

---
+++ src/main/java/br/com/wlbr/screensound/service/ConsultaChaGPT.java
@@ -0,0 +1,20 @@
+package br.com.wlbr.screensound.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+public class ConsultaChaGPT {
+    public static String ObterInformaçao(String txt){
+        OpenAiService service = new OpenAiService(System.getenv("OPENAI_APIKEY"));
+
+        CompletionRequest requisicao = CompletionRequest.builder()
+                .model("text-davince-003")
+                .prompt("me fale sobre o artista: " + txt)
+                .maxTokens(1000)
+                .temperature(0.7)
+                .build();
+
+        var reposta = service.createCompletion(requisicao);
+        return reposta.getChoices().get(0).getText();
+    }
+}
