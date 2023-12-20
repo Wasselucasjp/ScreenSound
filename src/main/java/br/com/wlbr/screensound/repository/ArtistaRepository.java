1b5f41677fe972dd3dc981ef492b3b08ce5b2463
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-20 13:21:56 -0300

    query para buscar musica de um artista

--- src/main/java/br/com/wlbr/screensound/repository/ArtistaRepository.java
+++ src/main/java/br/com/wlbr/screensound/repository/ArtistaRepository.java
@@ -1,10 +1,15 @@
 package br.com.wlbr.screensound.repository;
 
 import br.com.wlbr.screensound.model.Artista;
+import br.com.wlbr.screensound.model.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
     Optional<Artista> findByNomeContainingIgnoreCase(String nome);
+    @Query("SELECT m FROM Artista a JOIN a.musicas m WHERE a.nome ILIKE %:nome")
+    List<Musica> buscaMusicaPorArtista(String nome);
 }
